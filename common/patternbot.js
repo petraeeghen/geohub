b1e9b87ba16925dba39fd2a011fbd10f4c966009
petraeeghen <petraeeghen@gmail.com>
2018-03-20 12:45:06 -0400

    Add title to website

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/petravaneeghen/Documents/Documents - Petra’s MacBook Pro 2017/Semester 2/Semester 3/Semester 4/Web Dev 4/geohub
- * @version 1521564192562
+ * @version 1521564298521
  */
-const patternManifest_1521564192562 = {
+const patternManifest_1521564298521 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -846,5 +846,5 @@ const patternManifest_1521564192562 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521564192562);
+patternBotIncludes(patternManifest_1521564298521);
 }());
